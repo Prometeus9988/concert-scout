6be0e8c847a48fa2c65fb45726cf59b3e6e9b48f
ferra-rally <56554604+ferra-rally@users.noreply.github.com>
2019-11-13 17:32:43 +0100

    first-commit

---
+++ src/ferrarally/HelloWorld.java
@@ -0,0 +1,12 @@
+package ferrarally;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloWorld {
+	private static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
+
+	public static void main(String[] args) {
+		logger.log(Level.INFO, "Hello World!");
+	}
+}
