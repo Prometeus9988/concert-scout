2f9160086f459684bc3e7e9d2bf2b7f637e81730
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-02-12 15:05:09 +0100

    LIVEtheMUSIC-156 Closes #156

--- src/logic/dao/MusicEventDao.java
+++ src/logic/dao/MusicEventDao.java
@@ -10,15 +10,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import logic.entity.MusicEvent;
 import logic.utils.*;
 
-public class MusicEventDao {
-	
-	private static final Logger logger = Logger.getLogger(MusicEventDao.class.getName());
+public class MusicEventDao extends DaoTemplate {
+
 	private static final String ID = "id";
 	private static final String NAME = "name";
 	private static final String LOCATION = "location";
@@ -43,68 +40,54 @@ public class MusicEventDao {
 	private int rng = 0;
 	
 	public MusicEvent getMusicEvent(String id, String role) {
-		Connection conn = null;
-		MusicEvent me = null;
-		ResultSet rs = null;
-		String sql = null;
-		PreparedStatement stm = null;
-		
-		try {
-			if(role.equals("admin")) {
-				conn = DBAdminConnection.getAdminConnection();
-				sql = "call livethemusic.get_pending_musicevent(?);\r\n";
-			} else {
-				conn = DBUserConnection.getUserConnection();
-		        sql = "call livethemusic.get_musicevent(?);\r\n";
-			}
+		return this.execute(new DaoAction<MusicEvent>() {
+			@Override
+			public MusicEvent execute() throws ClassNotFoundException, SQLException {
+				Connection conn = null;
+				MusicEvent me = null;
+				String sql = null;
 
-			stm = conn.prepareStatement(sql);
-			
-	        stm.setString(1, id);
-			rs = stm.executeQuery();
-			rs.next();
-			
-            int idb = rs.getInt(ID);
-            String name = rs.getString(NAME);
-            String location = rs.getString(LOCATION);
-            String bandName = rs.getString(BANDNAME);
-            String artistUsername = rs.getString(ARTISTUSERNAME);
-            String ticketone = rs.getString(TICKETONE);
-            String coverPath = rs.getString(COVERPATH);
-            
-            //setting up coordinates of the musicevent
-            double latitude = rs.getDouble(LATITUDE);
-            double longitude = rs.getDouble(LONGITUDE);
-            List<Double> coordinates = new ArrayList<>();
-            coordinates.add(latitude);
-            coordinates.add(longitude);
-            
-        	if(coverPath == null || coverPath.equals("")) {
-        		coverPath = DEFAULTPICTURE;
-        	}
+				if(role.equals("admin")) {
+					conn = DBAdminConnection.getAdminConnection();
+					sql = "call livethemusic.get_pending_musicevent(?);\r\n";
+				} else {
+					conn = DBUserConnection.getUserConnection();
+					sql = "call livethemusic.get_musicevent(?);\r\n";
+				}
+
+				try(PreparedStatement stm = conn.prepareStatement(sql)) {
+
+					stm.setString(1, id);
+					try (ResultSet rs = stm.executeQuery()) {
+						rs.next();
+
+						int idb = rs.getInt(ID);
+						String name = rs.getString(NAME);
+						String location = rs.getString(LOCATION);
+						String bandName = rs.getString(BANDNAME);
+						String artistUsername = rs.getString(ARTISTUSERNAME);
+						String ticketone = rs.getString(TICKETONE);
+						String coverPath = rs.getString(COVERPATH);
+
+						//setting up coordinates of the musicevent
+						double latitude = rs.getDouble(LATITUDE);
+						double longitude = rs.getDouble(LONGITUDE);
+						List<Double> coordinates = new ArrayList<>();
+						coordinates.add(latitude);
+						coordinates.add(longitude);
+
+						if(coverPath == null || coverPath.equals("")) {
+							coverPath = DEFAULTPICTURE;
+						}
         	
-            me = new MusicEvent(idb, artistUsername, name, coverPath, location, bandName, ticketone);
-            me.setCoordinates(coordinates);
-            
-        } catch (SQLException se) {
-        	logger.log(Level.WARNING, se.toString());
-        } catch (ClassNotFoundException e) {
-        	logger.log(Level.WARNING, e.toString());
-        } finally {
-        	try {
-                if (rs != null)
-                    rs.close();
-            } catch (SQLException se1) {
-            	logger.log(Level.WARNING, se1.toString());
-            }
-            try {
-                if (stm != null)
-                    stm.close();
-            } catch (SQLException se2) {
-            	logger.log(Level.WARNING, se2.toString());
-            }
-        }
-        return me;
+						me = new MusicEvent(idb, artistUsername, name, coverPath, location, bandName, ticketone);
+						me.setCoordinates(coordinates);
+					}
+				}
+				return me;
+			}
+        });
+
 	}
 	
 	public List<MusicEvent> getSuggestedEvents(String username){
@@ -127,83 +110,49 @@ public class MusicEventDao {
 		this.manageParticipation(username, musicEventId, REMOVEPART);
 	}
 	
-	public boolean isParticipating(String username, String musicEventId) {
-		Connection conn = null;
-		PreparedStatement stm = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DBUserConnection.getUserConnection();
-			String sql = "call livethemusic.is_participating(?, ?);\r\n"; 
-			stm = conn.prepareStatement(sql);
-	        stm.setString(1, username);
-	        stm.setInt(2,  Integer.parseInt(musicEventId));
-	       	rs = stm.executeQuery();
-            
-			if (!rs.first()) {
-				rs.close();
-				return false;
-			} else {
-				rs.close();
-				return true;
+	public Boolean isParticipating(String username, String musicEventId) {
+		Boolean ret = this.execute(new DaoAction<Boolean>() {
+			@Override
+			public Boolean execute() throws ClassNotFoundException, SQLException {
+				Connection conn = DBUserConnection.getUserConnection();
+				String sql = "call livethemusic.is_participating(?, ?);\r\n"; 
+				try (PreparedStatement stm = conn.prepareStatement(sql)) {
+					stm.setString(1, username);
+					stm.setInt(2,  Integer.parseInt(musicEventId));
+					try (ResultSet rs = stm.executeQuery()) {
+						return (rs.first());
+					}
+				}
 			}
-			
-		} catch (SQLException se) {
-        	logger.log(Level.WARNING, se.toString());
-        } catch (ClassNotFoundException e) {
-        	logger.log(Level.WARNING, e.toString());
-        } finally {
-        	try {
-                if (rs != null)
-                    rs.close();
-            } catch (SQLException se1) {
-            	logger.log(Level.WARNING, se1.toString());
-            }
-            try {
-                if (stm != null)
-                    stm.close();
-            } catch (SQLException se2) {
-            	logger.log(Level.WARNING, se2.toString());
-            }
-        }
-		return false;
+		});
+		if (ret != null)
+			return ret;
+		else
+			return false;
 	}
 	
 	public boolean addMusicEvent(String name, String coverPath, String location, String artistUsername, Date date, String ticketone, List<Double> coordinates) {
-    	Connection con = null;
-    	PreparedStatement stm = null;
-    	
-    	try {
-    		con = DBArtistConnection.getArtistConnection();
-    		String sql = "call livethemusic.add_music_event(?, ?, ?, ?, ?, ?, ?, ?);\r\n"; 
-    		stm = con.prepareStatement(sql);
-    		java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+		return (this.execute(new DaoAction<Boolean>() {
+			@Override
+			public Boolean execute() throws ClassNotFoundException, SQLException {
+				Connection con = DBArtistConnection.getArtistConnection();
+				String sql = "call livethemusic.add_music_event(?, ?, ?, ?, ?, ?, ?, ?);\r\n"; 
+				try (PreparedStatement stm = con.prepareStatement(sql)) {
+					java.sql.Date sqlDate = new java.sql.Date(date.getTime());
 
-    		stm.setString(1, name);
-    		stm.setString(2, coverPath);
-    		stm.setString(3, location);
-    		stm.setString(4, artistUsername);
-    		stm.setDate(5, sqlDate);
-    		stm.setString(6, ticketone);
-    		stm.setDouble(7, coordinates.get(0));
-    		stm.setDouble(8, coordinates.get(1));
-    		stm.executeUpdate();
-    		
-    	} catch (SQLException se) {
-        	logger.log(Level.WARNING, se.toString());
-        	return false;
-        } catch (ClassNotFoundException e) {
-            logger.log(Level.WARNING, e.toString());
-            return false;
-        } finally {
-            try {
-                if (stm != null)
-                    stm.close();
-            } catch (SQLException se2) {
-            	logger.log(Level.WARNING, se2.toString());
-            }
-        }
-    	return true;
+					stm.setString(1, name);
+					stm.setString(2, coverPath);
+					stm.setString(3, location);
+					stm.setString(4, artistUsername);
+					stm.setDate(5, sqlDate);
+					stm.setString(6, ticketone);
+					stm.setDouble(7, coordinates.get(0));
+					stm.setDouble(8, coordinates.get(1));
+					stm.executeUpdate();
+					return true;
+				}
+			}
+		}) != null);
 	}
 
 	public void acceptMusicEvent(String id) {
@@ -218,7 +167,7 @@ public class MusicEventDao {
 		return this.queryDataBase(username, "userevents");
 	}
 	
-	public List<MusicEvent> getAroundYou(double latitude, double longitude, int range){
+	public List<MusicEvent> getAroundYou(double latitude, double longitude, int range) {
 		lat = latitude;
 		lng = longitude;
 		rng = range;
@@ -235,68 +184,60 @@ public class MusicEventDao {
 	}
 	
 	//This method compacts three similar Data-Base queries
-	private List<MusicEvent> queryDataBase(String string, String type){
-		Connection conn = null;
-		List<MusicEvent> l = new ArrayList<>();
-		PreparedStatement stm = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DBUserConnection.getUserConnection();
+	private List<MusicEvent> queryDataBase(String string, String type) {
+		List<MusicEvent> ret = this.execute(new DaoAction<List<MusicEvent>>() {
+			@Override
+			public List<MusicEvent> execute() throws ClassNotFoundException, SQLException {
+				Connection conn = null;
+				List<MusicEvent> l = new ArrayList<>();
+				PreparedStatement stm = null;
+				try {
+					if(type.equals("search")) {
+						conn = DBUserConnection.getUserConnection();
+						String sql = "call livethemusic.search_music_event(?);\r\n"; 
+						stm = conn.prepareStatement(sql);
+						//SearchString
+						stm.setString(1, string);
+					} else if(type.equals("suggested")) {
+						conn = DBUserConnection.getUserConnection();
+						String sql = "call livethemusic.view_friend_partitipation(?);\r\n"; 
+						stm = conn.prepareStatement(sql);
+						//Username
+						stm.setString(1, string);
+					} else if(type.equals("pending")) {
+						conn = DBAdminConnection.getAdminConnection();
+						String sql = "call livethemusic.view_pendingmusicevent();\r\n"; 
+						stm = conn.prepareStatement(sql);
+					} else if(type.equals(AROUNDYOU)) {
+						conn = DBUserConnection.getUserConnection();
+						String sql = "call livethemusic.around_you(?, ?, ?);\r\n";
+						stm = conn.prepareStatement(sql);
+						stm.setDouble(1, lat);
+						stm.setDouble(2, lng);
+						stm.setInt(3, rng);
+					} else if(type.equals("userevents")) {
+						conn = DBUserConnection.getUserConnection();
+						String sql = "call livethemusic.view_user_events(?);\r\n";	
+						stm = conn.prepareStatement(sql);
+						stm.setString(1, string);
+					}
 			
-			if(type.equals("search")) {
-	            String sql = "call livethemusic.search_music_event(?);\r\n"; 
-	            stm = conn.prepareStatement(sql);
-	            //SearchString
-	            stm.setString(1, string);
-			} else if(type.equals("suggested")) {
-	    		String sql = "call livethemusic.view_friend_partitipation(?);\r\n"; 
-	    		stm = conn.prepareStatement(sql);
-	    		//Username
-	            stm.setString(1, string);
-			} else if(type.equals("pending")) {
-				conn = DBAdminConnection.getAdminConnection();
-
-				String sql = "call livethemusic.view_pendingmusicevent();\r\n"; 
-				stm = conn.prepareStatement(sql);
-			} else if(type.equals(AROUNDYOU)) {
-				String sql = "call livethemusic.around_you(?, ?, ?);\r\n";
-				
-				stm = conn.prepareStatement(sql);
-				stm.setDouble(1, lat);
-				stm.setDouble(2, lng);
-				stm.setInt(3, rng);
-			} else if(type.equals("userevents")) {
-				String sql = "call livethemusic.view_user_events(?);\r\n";
-				
-				stm = conn.prepareStatement(sql);
-				stm.setString(1, string);
+					if(stm != null) {
+						try (ResultSet rs = stm.executeQuery()) {
+							l = unpackResultSet(rs, type);
+						}
+					}
+				} finally {
+					if (stm != null)
+						stm.close();
+				}
+				return l;
 			}
-			
-			if(stm != null) {
-				rs = stm.executeQuery();
-			}
-			l = this.unpackResultSet(rs, type);
-		} catch (SQLException se) {
-        	logger.log(Level.WARNING, se.toString());
-        } catch (ClassNotFoundException e) {
-            logger.log(Level.WARNING, e.toString());
-        } finally {
-        	try {
-                if (rs != null)
-                    rs.close();
-            } catch (SQLException se1) {
-            	logger.log(Level.WARNING, se1.toString());
-            }
-            try {
-                if (stm != null)
-                    stm.close();
-            } catch (SQLException se2) {
-            	logger.log(Level.WARNING, se2.toString());
-            }
-        }
-		
-		return l;
+		});
+		if (ret != null)
+			return ret;
+		else
+			return Collections.emptyList();
 	}
 	
 	private List<MusicEvent> unpackResultSet(ResultSet rs, String type) throws SQLException{
@@ -313,6 +254,11 @@ public class MusicEventDao {
         	String artistUsername = rs.getString(ARTISTUSERNAME);
         	String coverPath = rs.getString(COVERPATH);
         	String ticketone = rs.getString(TICKETONE);
+			double latitude = rs.getDouble(LATITUDE);
+			double longitude = rs.getDouble(LONGITUDE);
+			List<Double> coordinates = new ArrayList<>();
+			coordinates.add(latitude);
+			coordinates.add(longitude);
 
         	if(coverPath == null || coverPath.equals("")) {
         		coverPath = DEFAULTPICTURE;
@@ -330,66 +276,61 @@ public class MusicEventDao {
 	}
 	
 	public void manageMusicEvent(String id, String operation) {
-		Connection con = null;
-    	PreparedStatement stm = null;
-    	try {
-    		if(operation.equals(REJECT)) {
-    			con = DBAdminConnection.getAdminConnection();
-    			String sql = "call livethemusic.reject_musicevent(?);\r\n"; 
-    			stm = con.prepareStatement(sql);
-    			stm.setString(1, id);
-    			
-    		} else if(operation.equals(ACCEPT)) {
-        		con = DBAdminConnection.getAdminConnection();
-        		String sql = "call livethemusic.accept_musicevent(?);\r\n"; 
-        		stm = con.prepareStatement(sql);
-                stm.setString(1, id);
-    		}
+		this.execute(new DaoAction<Void>() {
+			@Override
+			public Void execute() throws ClassNotFoundException, SQLException {
+				Connection con = null;
+				PreparedStatement stm = null;
+				try {
+					if(operation.equals(REJECT)) {
+						con = DBAdminConnection.getAdminConnection();
+						String sql = "call livethemusic.reject_musicevent(?);\r\n"; 
+						stm = con.prepareStatement(sql);
+						stm.setString(1, id);	
+					} else if(operation.equals(ACCEPT)) {
+						con = DBAdminConnection.getAdminConnection();
+						String sql = "call livethemusic.accept_musicevent(?);\r\n"; 
+						stm = con.prepareStatement(sql);
+						stm.setString(1, id);
+					}
     		
-    		if(stm != null) {
-    			stm.executeUpdate();
-    		}
-    		
-    	} catch (SQLException se) {
-        	logger.log(Level.WARNING, se.toString());
-        } catch (ClassNotFoundException e) {
-            logger.log(Level.WARNING, e.toString());
-        } finally {
-            try {
-                if (stm != null)
-                    stm.close();
-            } catch (SQLException se2) {
-            	logger.log(Level.WARNING, se2.toString());
-            }
-        }
+					if (stm != null) {
+						stm.executeUpdate();
+					}
+				} finally {
+					if (stm != null) {
+						stm.close();
+					}
+				}
+				return null;
+			}
+        });
 	}
 	
 	private void manageParticipation(String username, String musicEventId, String operation) {
-		Connection conn = null;
-		PreparedStatement stm = null;
-		String sql = null;
-		try {
-			conn = DBUserConnection.getUserConnection();
-			if(operation.equals(ADDPART)) {
-				sql = "call livethemusic.add_participation(?, ?);\r\n";
-			} else if(operation.equals(REMOVEPART)) {
-				sql = "call livethemusic.remove_participation(?, ?);\r\n"; 
-			}
-			stm = conn.prepareStatement(sql);
-	        stm.setString(1, username);
-	        stm.setInt(2,  Integer.parseInt(musicEventId));
-	        stm.executeUpdate();
-		} catch (SQLException se) {
-        	logger.log(Level.WARNING, se.toString());
-        } catch (ClassNotFoundException e) {
-        	logger.log(Level.WARNING, e.toString());
-        } finally {
-            try {
-                if (stm != null)
-                    stm.close();
-            } catch (SQLException se2) {
-            	logger.log(Level.WARNING, se2.toString());
+		this.execute(new DaoAction<Void>() {
+			@Override
+			public Void execute() throws ClassNotFoundException, SQLException {
+				Connection conn = null;
+				PreparedStatement stm = null;
+				String sql = null;
+				try {
+					conn = DBUserConnection.getUserConnection();
+					if(operation.equals(ADDPART)) {
+						sql = "call livethemusic.add_participation(?, ?);\r\n";
+					} else if(operation.equals(REMOVEPART)) {
+						sql = "call livethemusic.remove_participation(?, ?);\r\n"; 
+					}
+					stm = conn.prepareStatement(sql);
+					stm.setString(1, username);
+					stm.setInt(2,  Integer.parseInt(musicEventId));
+					stm.executeUpdate();
+				} finally {
+					if (stm != null)
+						stm.close();
+				}
+				return null;
             }
-        }
+        });
 	}
 }
