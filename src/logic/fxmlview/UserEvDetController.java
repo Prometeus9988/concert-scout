8affee981f711601853502bd55bc0d3516d2766a
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-02-14 12:17:02 +0100

    LIVEtheMUSIC-177 Closes #177

--- src/logic/fxmlview/UserEvDetController.java
+++ src/logic/fxmlview/UserEvDetController.java
@@ -4,15 +4,16 @@ import java.io.File;
 import logic.bean.MusicEventBean;
 import javafx.scene.image.Image;
 
-public class AdminEvDetController extends EvDetailsController {
+public class UserEvDetController extends EvDetailsController {
 
 	@Override
 	public void init(MusicEventBean myEvent, String from, String searchString){
 
-		AdminGraphicChange agc = AdminGraphicChange.getInstance();
+		UserGraphicChange ucg=UserGraphicChange.getInstance();
 		
 		//INIT MENU BAR
-		agc.menuBar(this.menuBar, from);
+		ucg.menuBar(this.menuBar, from);
+		
 		
 		//INIT LABEL NAMES
 		this.conc.setText(myEvent.getName());
@@ -20,22 +21,26 @@ public class AdminEvDetController extends EvDetailsController {
 		this.arName.setText(myEvent.getArtistId());
 		
 		//INIT BACK BUTTON
-		agc.backButton(this.backButton, from, searchString);
+		
+		ucg.backButton(this.backButton, from, searchString);
 		
 		//INIT ROLE BUTTONS
-		agc.evDetailsButtons(this.buttons, myEvent);
+		
+		ucg.evDetailsButtons(this.buttons, myEvent);
 		
 		//INIT IMAGE
 		String path=System.getProperty("user.home")+ File.separator
-					+ "Desktop" + File.separator + "LIVEtheMUSIC" + File.separator
-					+ "trunk" + File.separator + "WebContent" + File.separator+ "img"
-					+ File.separator + "concertPictures" + File.separator + myEvent.getCoverPath();
-				
+				+ "Desktop" + File.separator + "LIVEtheMUSIC" + File.separator
+				+ "trunk" + File.separator + "WebContent" + File.separator
+				+ "img" + File.separator + "concertPictures" + File.separator + myEvent.getCoverPath();
+		
 		File file = new File(path);
 		Image image = new Image(file.toURI().toString());
 		this.profileImage.setImage(image);
 		this.profileImage.setFitHeight(400);
 		this.profileImage.setFitWidth(1200);
-				
+		
 	}
+	
+
 }
