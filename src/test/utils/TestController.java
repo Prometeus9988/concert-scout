49ac771a9f8f4483918ae70d1bc6c5d16ca9cab6
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-02-17 20:30:03 +0100

    LIVEtheMUSIC-220 Closes #220

---
+++ src/test/utils/TestController.java
@@ -0,0 +1,52 @@
+package test.utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import logic.bean.MusicEventBean;
+import logic.buyticket.BuyTicketController;
+import logic.entity.MusicEvent;
+import logic.utils.Controller;
+
+public class TestController {
+	
+	private static final String TEST = "test";
+
+	@Test
+	public void testMusicEventBeanConvert() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+
+		List<Double> coordinates = new ArrayList<>();
+		coordinates.add(0.0);
+		coordinates.add(0.0);
+
+		// Build ad-hoc MusicEventBean
+		MusicEventBean meb1 = new MusicEventBean();
+		meb1.setId(42);
+		meb1.setArtistId(TEST);
+		meb1.setName(TEST);
+		meb1.setCoverPath(TEST);
+		meb1.setLocation(TEST);
+		meb1.setBandName(TEST);
+		meb1.setTicketone(TEST);
+		meb1.setLatitude(coordinates.get(0));
+		meb1.setLongitude(coordinates.get(1));
+
+		// Build MusicEvent
+		MusicEvent me = new MusicEvent(42, TEST, TEST, TEST, TEST, TEST, TEST);
+		me.setCoordinates(coordinates);
+
+		// Get method through reflection and call it
+		Method method = Controller.class.getDeclaredMethod("convert", MusicEvent.class);
+		method.setAccessible(true);
+		MusicEventBean meb2 = (MusicEventBean)method.invoke(new BuyTicketController(), me);
+
+		assertTrue(EqualsBuilder.reflectionEquals(meb1, meb2));
+	}
+
+}
