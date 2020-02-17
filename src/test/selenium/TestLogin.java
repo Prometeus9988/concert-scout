e646997fd6ba23ffc43cb76d4fb05e0667019b0d
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-02-17 23:48:05 +0100

    LIVEtheMUSIC-227 Closes #227

--- src/test/selenium/TestLogin.java
+++ src/test/selenium/TestLogin.java
@@ -9,26 +9,32 @@ import static org.junit.Assert.*;
 
 public class TestLogin {
 	
-	private static final String DRIVER="webdriver.chrome.driver";
+	private static final String DRIVER = "webdriver.chrome.driver";
+	private static final String LOGIN = "//*[@id=\"login\"]";
 	
-	@Test
-	public void testSuccessfullLogin() {
-		
-	
-		if(System.getProperty("os.name").equals("Linux")) {
+	private WebDriver setProps() {
+		if (System.getProperty("os.name").equals("Linux")) {
 			//LINUX
 			System.setProperty(DRIVER, "drivers/chromedriver");
-		}else {
+		} else {
 			//WINDOWS
 			System.setProperty(DRIVER, "Drivers/chromedriver.exe");
 		}
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://localhost:8080/LIVEtheMUSIC/");
+		return driver;
+	}
+	
+	@Test
+	public void testSuccessfulLogin() {
+		
+		WebDriver driver = this.setProps();
 
 		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("testusername1");
 		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("testusername1");
 		
-		driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+		driver.findElement(By.xpath(LOGIN)).click();
 		
 		WebElement welcomeText = driver.findElement(By.xpath("/html/body/div/div[2]/div/h2/i"));
 		
@@ -40,18 +46,10 @@ public class TestLogin {
 	@Test
 	public void testPasswordNecessaryLogin() {
 		
-		if(System.getProperty("os.name").equals("Linux")) {
-			//LINUX
-			System.setProperty(DRIVER, "drivers/chromedriver");
-		}else {
-			//WINDOWS
-			System.setProperty(DRIVER, "Drivers/chromedriver.exe");
-		}
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://localhost:8080/LIVEtheMUSIC/");
+		WebDriver driver = this.setProps();
 		
 		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("test");
-		driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+		driver.findElement(By.xpath(LOGIN)).click();
 		
 		WebElement label=driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/h6"));
 		
@@ -62,4 +60,22 @@ public class TestLogin {
 		assertEquals(attended,output);
 		
 	}
+	
+	@Test
+	public void testUsernameNecessaryLogin() {
+
+		WebDriver driver = this.setProps();
+
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("qwerty");
+		driver.findElement(By.xpath(LOGIN)).click();
+
+		WebElement label = driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/h6"));
+
+		String output = label.getText();
+		driver.close();
+		String attended = "Username necessary";
+		assertEquals(attended, output);
+
+	}
+
 }
