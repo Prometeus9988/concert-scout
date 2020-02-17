99862729e21f242aa01b3e59d76ad400e5f62bac
Lorenzo Valeriani <lorenzoval98@gmail.com>
2020-02-17 23:08:27 +0100

    LIVEtheMUSIC-226 Closes #226

---
+++ src/test/addnews/TestAddNewsController.java
@@ -0,0 +1,38 @@
+package test.addnews;
+
+import org.junit.Test;
+
+import logic.addnews.AddNewsController;
+import logic.bean.NewsBean;
+import logic.exceptions.FieldTooLongException;
+
+public class TestAddNewsController {
+
+	private static final String TEST = "test";
+
+	// it is safe to use this annotation because exception can be thrown only at line 35
+	@Test(expected = FieldTooLongException.class)
+	public void testAddNewsTooLong() throws FieldTooLongException {
+		AddNewsController nc = new AddNewsController();
+
+		// create a String with 200 chars
+		StringBuilder bld = new StringBuilder();
+		for (int i = 0; i < 200; i++) {
+			bld.append("a");
+		}
+
+		// build NewsBean
+		NewsBean nb = new NewsBean();
+		nb.setId(42);
+		nb.setArtistId(TEST);
+		nb.setBandName(TEST);
+		nb.setPicturePath(TEST);
+		nb.setPostedSince(TEST);
+		nb.setProfilePath(TEST);
+		nb.setText(bld.toString());
+
+		// call method
+		nc.addNews(nb);
+	}
+
+}
---
+++ src/test/readnews/TestReadNewsController.java
@@ -0,0 +1,29 @@
+package test.readnews;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import org.junit.Test;
+
+import logic.readnews.ReadNewsController;
+
+public class TestReadNewsController {
+	
+	@Test
+	public void testGetYears() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		// set LocalDateTime variables for test
+		LocalDateTime aDateTime = LocalDateTime.of(2020, Month.FEBRUARY, 17, 23, 00, 40);
+		LocalDateTime anotherDateTime = LocalDateTime.of(1998, Month.OCTOBER, 06, 12, 30, 40);
+
+		Method method = ReadNewsController.class.getDeclaredMethod("getYears", LocalDateTime.class, LocalDateTime.class);
+		method.setAccessible(true);
+		String time = (String)method.invoke(new ReadNewsController(), aDateTime, anotherDateTime);
+		
+		assertEquals("21years", time);
+	}
+
+}
