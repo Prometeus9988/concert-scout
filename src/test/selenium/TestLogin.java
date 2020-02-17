34f2680a80ef1a62076875d84dc7b85894d5e842
IronMarken <56649439+IronMarken@users.noreply.github.com>
2020-02-17 21:44:15 +0100

    LIVEtheMUSIC-223

--- src/test/selenium/TestLogin.java
+++ src/test/selenium/TestLogin.java
@@ -31,7 +31,33 @@ public class TestLogin {
 		WebElement welcomeText = driver.findElement(By.xpath("/html/body/div/div[2]/div/h2/i"));
 		
 		String value = welcomeText.getText();
-		
+		driver.close();
 		assertEquals("Welcome testusername1", value);
+	}
+	
+	@Test
+	public void testPasswordNecessaryLogin() {
+		
+		if(System.getProperty("os.name").equals("Linux")) {
+			//LINUX
+			System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+		}else {
+			//WINDOWS
+			System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		}
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://localhost:8080/LIVEtheMUSIC/");
+		
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("test");
+		driver.findElement(By.xpath("//*[@id=\"login\"]")).click();
+		
+		WebElement label=driver.findElement(By.xpath("/html/body/div/div[1]/div[2]/h6"));
+		
+		
+		String output=label.getText();
+		driver.close();
+		String attended="Password necessary";
+		assertEquals(attended,output);
+		
 	}
 }
