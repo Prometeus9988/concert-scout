3e671d77230ef8c9153a7ccc1144b763aefc3572
ferra-rally <56554604+ferra-rally@users.noreply.github.com>
2020-02-17 10:12:53 +0100

    LIVEtheMUSIC-215 Closes #215

---
+++ src/test/utils/TestGoogleMapBoundary.java
@@ -0,0 +1,30 @@
+package test.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import logic.utils.GoogleMapBoundary;
+
+public class TestGoogleMapBoundary {
+	
+	@Test
+	public void testLocateAddress() {
+		List<Double> coordinates = new ArrayList<>();
+		List<Double> target = new ArrayList<>();
+		
+		//Coordinates of Rome
+		target.add(41.9056798);
+		target.add(12.4822062);
+		
+		try {
+			coordinates = GoogleMapBoundary.locateAddress("Piazza di Spagna, Roma, Italia");
+		} catch (Exception e) {
+			fail("Should not have thrown any exception");
+		}
+		
+		assertEquals(target, coordinates);
+	}
+}
